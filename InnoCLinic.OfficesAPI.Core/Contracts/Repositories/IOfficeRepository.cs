ec30ac2d9453808616255278df726195d43406a0
S9392929 <apk59955@gmail.com>
2022-12-08 12:03:36 +0300

    Add project files.

---
+++ InnoCLinic.OfficesAPI.Core/Contracts/Repositories/IOfficeRepository.cs
@@ -0,0 +1,17 @@
+﻿using InnoCLinic.OfficesAPI.Core.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoCLinic.OfficesAPI.Core.Contracts.Repositories
+{
+    public interface IOfficeRepository
+    {
+        Task<IEnumerable<Office>> GetAllOfficesAsync();
+        Task<Office> GetOfficeAsync(Guid officeId);
+        Task CreateOfficeAsync(Office office);
+        Task DeleteOfficeAsync(Office office);
+    }
+}
---
+++ InnoCLinic.OfficesAPI.Core/Entities/Models/Office.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnoCLinic.OfficesAPI.Core.Entities.Models
+{
+    public class Office
+    {
+        public Guid Id { get; set; }
+        public string Address { get; set; }
+        public Guid PhotoId { get; set; }
+        public string Url { get; set; }
+        public string RegistryPhoneNumber { get; set; }
+        public string IsActive { get; set; }
+    }
+}
